f86919096cd811012d93790fb6b4a8af9ca76c44
NotBrightLight <aintnobodysafe117@gmail.com>
2025-06-03 13:59:09 +0200

    Image compressor crash fix

--- Packages/Environment/Sources/Environment/Compressor.swift
+++ Packages/Environment/Sources/Environment/Compressor.swift
@@ -19,54 +19,52 @@ public final class Compressor {
     public func compressImageForUpload(
         _ image: UIImage,
         maxSize: Int = 5 * 1024 * 1024,
-        maxHeight: Double = 5000,
-        maxWidth: Double = 5000
+        maxHeight: CGFloat = 3000,
+        maxWidth: CGFloat = 3000
     ) async throws -> Data {
-        var image = image
-        
-        if image.size.height > maxHeight || image.size.width > maxWidth {
-            let heightFactor = image.size.height / maxHeight
-            let widthFactor = image.size.width / maxWidth
-            let maxFactor = max(heightFactor, widthFactor)
-            
-            image = resize(
-                image: image,
-                to: .init(
-                    width: image.size.width / maxFactor,
-                    height: image.size.height / maxFactor
-                )
-            )
-        }
-        
-        guard var imageData = image.jpegData(compressionQuality: 0.8) else {
-            throw CompressorError.noData
-        }
-        
-        var compressionQualityFactor: CGFloat = 0.8
-        if imageData.count > maxSize {
-            while imageData.count > maxSize && compressionQualityFactor >= 0 {
-                guard let compressedImage = UIImage(data: imageData),
-                      let compressedData = compressedImage.jpegData(
-                        compressionQuality: compressionQualityFactor)
-                else {
-                    throw CompressorError.noData
+        return try await withCheckedThrowingContinuation { continuation in
+            DispatchQueue.global(qos: .utility).async {
+                autoreleasepool {
+                    var resizedImage = image
+                    if image.size.height > maxHeight || image.size.width > maxWidth {
+                        let heightFactor = image.size.height / maxHeight
+                        let widthFactor = image.size.width / maxWidth
+                        let scaleFactor = max(heightFactor, widthFactor)
+                        let newSize = CGSize(
+                            width: image.size.width / scaleFactor,
+                            height: image.size.height / scaleFactor
+                        )
+                        resizedImage = self.resize(image: image, to: newSize)
+                    }
+                    
+                    var quality: CGFloat = 0.9
+                    guard var data = resizedImage.jpegData(compressionQuality: quality) else {
+                        continuation.resume(throwing: CompressorError.noData)
+                        return
+                    }
+                    
+                    while data.count > maxSize && quality > 0.1 {
+                        quality -= 0.1
+                        if let newData = resizedImage.jpegData(compressionQuality: quality) {
+                            data = newData
+                        }
+                    }
+                    
+                    if data.count > maxSize {
+                        continuation.resume(throwing: CompressorError.noData)
+                    } else {
+                        continuation.resume(returning: data)
+                    }
                 }
-                
-                imageData = compressedData
-                compressionQualityFactor -= 0.1
             }
         }
-        
-        if imageData.count > maxSize && compressionQualityFactor <= 0 {
-            print(imageData)
-            throw CompressorError.noData
-        }
-        
-        return imageData
     }
     
     private func resize(image: UIImage, to size: CGSize) -> UIImage {
-        UIGraphicsImageRenderer(size: size).image { _ in
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = image.scale
+        format.opaque = true
+        return UIGraphicsImageRenderer(size: size, format: format).image { _ in
             image.draw(in: CGRect(origin: .zero, size: size))
         }
     }
