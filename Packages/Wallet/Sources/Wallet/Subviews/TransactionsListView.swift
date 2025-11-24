a2bb6a174ed98d579854cbaa5908eabac7cf878c
NotBrightLight <aintnobodysafe117@gmail.com>
2025-11-24 16:13:35 +0100

    Pin transactions header when scrolling

--- Packages/Wallet/Sources/Wallet/Subviews/TransactionsListView.swift
+++ Packages/Wallet/Sources/Wallet/Subviews/TransactionsListView.swift
@@ -13,32 +13,42 @@ struct TransactionsListView: View {
 
     var body: some View {
         VStack(spacing: 0) {
-            Button {
-                arrowAction()
-            } label: {
-                HStack(spacing: 0) {
-                    Text("Transactions")
-                        .appFont(.bodyBold)
-
-                    Spacer()
-                        .frame(minWidth: 0)
-                        .frame(maxWidth: .infinity)
-                        .layoutPriority(-1)
-
-                    Icons.arrowDown
-                        .iconSize(height: 8)
+            LazyVStack(spacing: 10, pinnedViews: .sectionHeaders) {
+                Section {
+                    ForEach(0..<100) { i in
+                        Text("Example \(i)")
+                            .font(.title)
+                            .frame(width: 200, height: 200)
+                            .background(Colors.passwordBarsGreen)
+                    }
+                } header: {
+                    transactionHistoryHeaderView
+                        .background(Colors.blackDark)
                 }
-                .foregroundStyle(Colors.whitePrimary)
-                .padding(.vertical, 10)
-                .contentShape(.rect)
-            }
 
-            ForEach(0..<100) { i in
-                Text("Example \(i)")
-                    .font(.title)
-                    .frame(width: 200, height: 200)
-                    .background(Colors.passwordBarsGreen)
             }
+        }
+    }
+
+    private var transactionHistoryHeaderView: some View {
+        Button {
+            arrowAction()
+        } label: {
+            HStack(spacing: 0) {
+                Text("Transactions")
+                    .appFont(.bodyBold)
+
+                Spacer()
+                    .frame(minWidth: 0)
+                    .frame(maxWidth: .infinity)
+                    .layoutPriority(-1)
+
+                Icons.arrowDown
+                    .iconSize(height: 8)
+            }
+            .foregroundStyle(Colors.whitePrimary)
+            .padding(.vertical, 10)
+            .contentShape(.rect)
         }
     }
 }
