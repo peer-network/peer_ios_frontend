7e6617dbe6514238a432a0a89c0a5259098eab98
NotBrightLight <aintnobodysafe117@gmail.com>
2025-12-29 09:34:23 +0100

    Remove auto generated packages

--- ApolloGQL/Packages/GQLOperationsUser/Sources/Schema/Enums/AdvertisementType.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public enum AdvertisementType: String, EnumType {
-  case pinned = "PINNED"
-  case basic = "BASIC"
-}
--- ApolloGQL/Packages/GQLOperationsUser/Sources/Schema/Enums/ContentFilterType.graphql.swift
+++
@@ -1,9 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public enum ContentFilterType: String, EnumType {
-  case mygrandmalikes = "MYGRANDMALIKES"
-  case mygrandmahates = "MYGRANDMAHATES"
-}
