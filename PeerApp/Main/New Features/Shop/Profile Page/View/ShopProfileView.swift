31b7cb9ffa952a1e3fa2c14258526dfe6a1d97ba
NotBrightLight <aintnobodysafe117@gmail.com>
2026-01-05 16:46:55 +0100

    Add paddings and placeholders for shop page view

--- PeerApp/Main/New Features/Shop/Profile Page/View/ShopProfileView.swift
+++ PeerApp/Main/New Features/Shop/Profile Page/View/ShopProfileView.swift
@@ -13,10 +13,10 @@ import Environment
 
 struct ShopProfileView: View {
     @EnvironmentObject private var apiManager: APIServiceManager
-    
+
     @StateObject private var viewModel: ProfileViewModel
 
-    @State private var typeSelected = ShopItemsDisplayType.list
+    @State private var layout = ShopItemsDisplayType.list
 
     init() {
         _viewModel = .init(wrappedValue: .init(userId: "4cca9cfe-762b-416f-8e15-571f4d6798c9"))
@@ -42,25 +42,15 @@ struct ShopProfileView: View {
             ScrollView {
                 LazyVStack(alignment: .center, spacing: 0, pinnedViews: [.sectionHeaders]) {
                     Section {
-                        LazyVStack(spacing: 8) {
-                            ForEach(0...100, id: \.self) { idx in
-                                Rectangle()
-                                    .fill(Colors.inactiveDark)
-                                    .frame(height: 120)
-                                    .clipShape(RoundedRectangle(cornerRadius: 16))
-                                    .overlay(
-                                        Text("Post \(idx)")
-                                            .appFont(.bodyRegular)
-                                            .foregroundStyle(Colors.whitePrimary.opacity(0.7))
-                                    )
-                            }
-                        }
+                        postsView
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 20)
                     } header: {
                         VStack(spacing: 10) {
                             headerView(user: user)
                                 .padding(.horizontal, 20)
 
-                            ShopTabControllerView(type: $typeSelected)
+                            ShopTabControllerView(type: $layout)
                         }
                         .padding(.top, 20)
                         .background(Colors.blackDark)
@@ -68,6 +58,7 @@ struct ShopProfileView: View {
 
                 }
             }
+            .scrollIndicators(.hidden)
         } else {
             ProgressView()
         }
@@ -108,6 +99,43 @@ struct ShopProfileView: View {
         .padding(10)
         .background(Color(red: 0, green: 0.41, blue: 1))
         .clipShape(RoundedRectangle(cornerRadius: 24))
+    }
+
+    @ViewBuilder
+    private var postsView: some View {
+        let items = Array(0..<60)
+
+        switch layout {
+            case .grid:
+                let cols = Array(repeating: GridItem(.flexible(), spacing: 10), count: 2)
+                LazyVGrid(columns: cols, spacing: 10) {
+                    ForEach(items, id: \.self) { idx in
+                        Rectangle()
+                            .fill(Colors.inactiveDark)
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(
+                                Text("\(idx)")
+                                    .appFont(.smallLabelRegular)
+                                    .foregroundStyle(Colors.whitePrimary.opacity(0.7))
+                            )
+                    }
+                }
+
+            case .list:
+                LazyVStack(spacing: 10) {
+                    ForEach(items, id: \.self) { idx in
+                        Rectangle()
+                            .fill(Colors.inactiveDark)
+                            .frame(height: 120)
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                            .overlay(
+                                Text("Post \(idx)")
+                                    .appFont(.bodyRegular)
+                                    .foregroundStyle(Colors.whitePrimary.opacity(0.7))
+                            )
+                    }
+                }
+        }
     }
 
     private func fetchEverything() async {
